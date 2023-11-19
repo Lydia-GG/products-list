d315418d738a3b8f286f61700b3e0f9c2453c856
Lydia Samir <lolyeg83@hotmail.com>
2023-11-19 21:23:41 +0100

    finished task 2 intern2grow

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,38 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class addItem extends Component {
-    state = {
-        product: '',
-        price: ''
-    }
+  state = {
+    product: '',
+    price: '',
+    quantity: 0,
+  };
 
-    handleChange = (e) => {
-        console.log(e.target.id + ": " + e.target.value)
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+  handleChange = (e) => {
+    console.log(e.target.id + ': ' + e.target.value);
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.add(this.state)
-        this.setState({
-            product: '',
-            price: ''
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.add(this.state);
+    this.setState({
+      product: '',
+      price: '',
+      quantity: 0,
+    });
+  };
+  increase = (e) => {
+    e.preventDefault();
+    this.setState({ ...this.state, quantity: this.state.quantity + 1 });
+  };
+  decrease = (e) => {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      quantity: this.state.quantity > 0 ? this.state.quantity - 1 : 0,
+    });
+  };
+  render() {
+    return (
+      <div className="item">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.product}
+            placeholder="Enter Product"
+            id="product"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="number"
+            value={this.state.price}
+            placeholder="Enter Price"
+            id="price"
+            onChange={this.handleChange}
+            required
+          />
+          <div className="quantity-input">
+            <button onClick={this.increase}>+</button>
+            <input
+              value={this.state.quantity}
+              id="quantity"
+              onChange={this.handleChange}
+              required
+            />
+            <button onClick={this.decrease}>-</button>
+          </div>
 
-    render(){
-        return(
-            <div className="item">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
-                </form>
-            </div>
-        )
-    }
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default addItem;
--- src/components/total/total.js
+++ src/components/total/total.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const Total = (props) => {
-    const {items} = props;
-    let total = 0
-    for (let i = 0; i < items.length; i++) {
-        total += parseFloat(items[i].price)
-    }
-    return (
-        <div>
-            <p className="text">Total Price: {total}</p>
-        </div>
-    )
-}
+  const { items } = props;
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    total += parseFloat(items[i].quantity) * parseFloat(items[i].price);
+  }
 
-export default Total
+  return (
+    <div>
+      <p className="text">Total Price: {total}</p>
+    </div>
+  );
+};
+
+export default Total;
